139d8e26ad253a60324fba4616e3862fe61da7ba
Yonas Hiko <yonashumna@gmail.com>
2021-04-09 13:46:03 -0400

    task-three

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
+
+
 class CampsiteInfo extends Component {
     renderCampsite(campsite){
         return (
@@ -16,11 +18,26 @@ class CampsiteInfo extends Component {
         )
     }
 
+    renderComments(comments){
+        if(comments) {
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comments => <div key={comments.id}>{comments.text}<br/>
+                    {comments.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</div>)}
+                </div>
+            )
+        };
+        return <div />
+    }
+
+
     render() {
         if(this.props.campsite) {
             return (
                 <div className="row">
             {this.renderCampsite(this.props.campsite)}
+            {this.renderComments(this.props.campsite.comments)}
                 </div>
             );
         }
@@ -29,4 +46,6 @@ class CampsiteInfo extends Component {
         )
     }
 }
+
+
 export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -39,5 +39,6 @@ class Directory extends Component {
         );
     }
 }
-//
+
+
 export default Directory;
